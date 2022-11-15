ea50971d3a9287155d446fe6a6e9b6a6b482c263
Najee Reno <nereno96@gmail.com>
2022-11-15 12:22:57 -0500

    completed basic functions for mountains.html

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,8 +1,10 @@
 "use strict"
 
 const mountainsList = document.getElementById("mountainsList");
+const mountainInfo = document.getElementById("mountainInfo");
 let option = new Option("Select a mountain", "select");
 mountainsList.appendChild(option);
+
 window.onload = function() {
     initMountainsList();
     mountainsList.onchange = displayInfo;
@@ -10,11 +12,20 @@ window.onload = function() {
 
 function initMountainsList() {
     for (let mountain of mountainsArray) {
-        let option = new Option(mountain.name, mountain.img);
+        let option = new Option(mountain.name, mountain.name);
         mountainsList.appendChild(option);
     }
 }
 
 function displayInfo() {
     
+    for (let mountain of mountainsArray) {
+        if (mountainsList.value == "select") {
+            mountainInfo.innerHTML = "";
+        }
+        else if (mountain.name == mountainsList.value) {
+            mountainInfo.innerHTML += "<br>" + mountain.desc + "<br><img src='images/" + mountain.img + "' width='100%'></img>";
+        }
+    }
 }
+
