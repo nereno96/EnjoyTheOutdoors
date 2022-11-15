3e45c2b155d7a745818c4a5c8e8bfd53f469ff7c
Najee Reno <nereno96@gmail.com>
2022-11-15 09:32:03 -0500

    dropdowns in park.html have all basic functions

--- scripts/parks.js
+++ scripts/parks.js
@@ -18,10 +18,15 @@ window.onload = function() {
 // initializes the location and park type list
 function initLists() {
 // consider making the location and parkType lists a singular select and populating it based on the filter selected instead of two separate selects that are displayed when their filter is chosen
+
+    let locationOption = new Option("Select a location", "select");
+    locationsList.appendChild(locationOption);
     for (let location of locationsArray) {
         let option = new Option(location, location);
         locationsList.appendChild(option);
     }
+    let parkTypeOption = new Option("Select a park type", "select");
+    parkTypeList.appendChild(parkTypeOption);
     for (let parkType of parkTypesArray) {
         let option = new Option(parkType, parkType);
         parkTypeList.appendChild(option);
@@ -49,25 +54,39 @@ function displayList() {
 // populates parkList based on location chosen
 function parksByLocation() {
     parkList.length = 0;
+    let parkOption = new Option("Select a park", "select");
+    parkList.appendChild(parkOption);
     for (let park of nationalParksArray) {
         if (locationsList.value == park.State) {
             let option = new Option(park.LocationName, park.LocationID);
             parkList.appendChild(option);
         }
     }
-    parkList.style.display = "block"
+    if(locationsList.value != "select") {
+        parkList.style.display = "block"
+    }
+    else {
+        parkList.style.display = "none"
+    }
 }
 
 // populates parkList based on parkType chosen
 function parksByType() {
     parkList.length = 0;
+    let parkOption = new Option("Select a park", "select");
+    parkList.appendChild(parkOption);
     for (let park of nationalParksArray) {
         if (park.LocationName.indexOf(parkTypeList.value) != -1) {
             let option = new Option(park.LocationName, park.LocationID);
             parkList.appendChild(option);
         }
     }
-    parkList.style.display = "block"
+    if(parkTypeList.value != "select") {
+        parkList.style.display = "block"
+    }
+    else {
+        parkList.style.display = "none"
+    }
 }
 
 function displayParkInfo() {
@@ -75,5 +94,11 @@ function displayParkInfo() {
         if (parkList.value == park.LocationID) {
             parkInfoPara.innerHTML = "<span class='fw-bold'>Mailing Address: </span><br>" + park.LocationName + " (" + park.LocationID + ")  <br>" + park.Address + "<br>" + park.City + ", " + park.State + " " + park.ZipCode + "<br><span class='fw-bold'>Phone Number:</span> " + park.Phone + "<br><span class='fw-bold'>Fax Number:</span> " + park.Fax + "<br><span class='fw-bold'>Coordinates:</span> " + park.Latitude + ", " + park.Longitude;
         }
+    }
+    if(parkList.value != "select") {
+        parkInfoPara.style.display = "block"
+    }
+    else {
+        parkInfoPara.style.display = "none"
     }
 }
